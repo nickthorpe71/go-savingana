eeef42fc27ec35d3b70f1b3b1107dda6a01f20ee
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-08 17:21:39 -0600

    last attempt before starting down a nother path of tutials

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -13,26 +11,50 @@ const (
 
 func main() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		fmt.Println("initializing SDL:", err)
-		return
+		panic(err)
 	}
+	defer sdl.Quit()
 
-	window, err := sdl.CreateWindow(
-		"Saving Ana",
-		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		screenWidth, screenHeight,
-		sdl.WINDOW_OPENGL)
-
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight, sdl.WINDOW_SHOWN)
 	if err != nil {
-		fmt.Println("initializing window", err)
-		return
+		panic(err)
 	}
 	defer window.Destroy()
 
 	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
-		fmt.Println("initializing renderer:", err)
+		panic(err)
 	}
+
 	defer renderer.Destroy()
+
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, screenWidth, screenHeight)
+	if err != nil {
+		panic(err)
+	}
+	defer tex.Destroy()
+
+	pixels := make([]byte, screenWidth*screenHeight*4)
+
+	for y := 0; y < screenHeight; y++ {
+		for x := 0; x < screenWidth; x++ {
+			setPixel(x, y, color{byte(x % 255), byte(y % 255), 0}, pixels)
+		}
+	}
+
+	tex.Update(nil, pixels, screenWidth*4)
+	renderer.Copy(tex, nil, nil)
+	renderer.Present()
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+		sdl.Delay(16)
+	}
 
 }
